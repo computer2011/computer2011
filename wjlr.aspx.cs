32895bc9797cc078568a3dfb40fea9d5ee04a940
luochaojun <xiyuedeshijie@163.com>
2014-05-12 15:49:22 +0800

    add

--- wjlr.aspx.cs
+++ wjlr.aspx.cs
@@ -33,25 +33,24 @@ namespace WJDC
             SqlCommand cmd = new SqlCommand("INSERT INTO Wj (Wjm,Sno) VALUES ('" + Mctxt.Text.Trim() + "','" + snotxt.Text.Trim() + "')", cn);
             try
             {
-                    cn.Open();
-                 
-                    int val = cmd.ExecuteNonQuery();
-                  
-                    cn.Close();
-                    if (val <= 0)
-                        ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败!')");
-                    else
-                        ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据成功!')");
-                
+                cn.Open();
+
+                int val = cmd.ExecuteNonQuery();
+
+                cn.Close();
+                if (val <= 0)
+                    ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败!')");
+                else
+                    ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据成功!')");
+
             }
             //捕获异常 
             catch (Exception exp)
             {
                 //处理异常....... 
                 ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败! 详情:" + exp.Message + "')");
-               
-                    
             }
+           
         }
 
         protected void Tmbtn_Click(object sender, EventArgs e)
@@ -59,18 +58,22 @@ namespace WJDC
             
             SqlCommand cmd = new SqlCommand("select MAX(wjh) from wj", cn);
             cn.Open();
-            cmd.ExecuteScalar();  
+            cmd.ExecuteScalar();
+            //cn.Close();
             SqlCommand com = new SqlCommand("INSERT INTO Tm(Wjh,Tm,Tixing) VALUES (" +cmd.ExecuteScalar()+ ",'" + Tmtxt.Text.Trim() + "','" + TXDropDownList.Text.Trim() + "')", cn);
-            
+            //com.ExecuteNonQuery();
+            //cn.Close();
             try
             {
-                cn.Open();
-                int val = cmd.ExecuteNonQuery();
+                //cn.Open();
+                int val = com.ExecuteNonQuery();
+                cn.Close();
                 this.NRListBox.Items.Add(this.Tmtxt.Text);
                 if (val <= 0)
                     ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败!')");
                 else
                     ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据成功!')");
+              
             }
             //捕获异常 
             catch (Exception exp)
@@ -78,8 +81,8 @@ namespace WJDC
                 //处理异常....... 
                 ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败! 详情:" + exp.Message + "')");
 
-
             }
+           
         }
        
 
@@ -89,11 +92,13 @@ namespace WJDC
             SqlCommand cmd = new SqlCommand("select MAX(Th) from Tm", cn);
             cn.Open();
             cmd.ExecuteScalar();           
-            SqlCommand com = new SqlCommand("INSERT INTO Choice(Th,choice) VALUES (" + cmd.ExecuteScalar() + ",'" + XXtxt.Text.Trim() + "')", cn);                     
+            SqlCommand com = new SqlCommand("INSERT INTO Choice(Th,choice) VALUES (" + cmd.ExecuteScalar() + ",'" + XXtxt.Text.Trim() + "')", cn);
+            //com.ExecuteNonQuery();
             try
             {
-                cn.Open();
-                int val = cmd.ExecuteNonQuery();
+                //cn.Open();
+                int val = com.ExecuteNonQuery();
+                cn.Close();
                 this.NRListBox.Items.Add(this.XXtxt.Text);              
                 if (val <= 0)
                     ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败!')");
@@ -107,7 +112,7 @@ namespace WJDC
                 //处理异常....... 
                 ClientScript.RegisterStartupScript(this.GetType(), "", "alert('插入数据失败! 详情:" + exp.Message + "')");
 
-
+               
             }
         }
         protected void homeLinkButton_Click(object sender, EventArgs e)
