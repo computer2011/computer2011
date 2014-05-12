f9a45159fd64e0e546a7083e1c653415dbb5f362
DongQiPeng <747859839@qq.com>
2014-05-12 20:04:36 +0800

    20:04dqp

--- IndexPage.aspx.cs
+++ IndexPage.aspx.cs
@@ -16,7 +16,6 @@ namespace computer2011
         {
             if (!IsPostBack)
             {
-
                 DataTable table1 = new DataTable();
                 SqlDataAdapter da1 = new SqlDataAdapter("select top 1 text from MainPage where LBID=1 order by ID desc", cn);//班级简介
                 da1.Fill(table1);
@@ -24,70 +23,66 @@ namespace computer2011
                 this.GridView1.DataBind();
 
                 #region
-                SqlDataAdapter da = new SqlDataAdapter("select  ID,LBID,Title from MainPage ", cn);
-                DataSet ds = new DataSet();
-                da.Fill(ds, "table");
+                //SqlDataAdapter da = new SqlDataAdapter("select  ID,LBID,Title from MainPage ", cn);
+                //DataSet ds = new DataSet();
+                //da.Fill(ds, "table");
 
 
-                DataView DV1 = new DataView();//最新班级新闻
-                DV1.Table = ds.Tables["table"];
-                DV1.Sort = "ID desc";
-                DV1.RowFilter = "LBID=2";
-                this.GridView2.DataSource = DV1;
-                this.GridView2.DataBind();
-
-                DataView DV2 = new DataView();//最新学习资料
-                DV2.Table = ds.Tables["table"];
-                DV2.Sort = "ID desc";
-                DV2.RowFilter = "LBID=3";
-                this.GridView3.DataSource = DV2;
-                this.GridView3.DataBind();
-
-                DataView DV3 = new DataView();//班级活动回顾
-                DV3.Table = ds.Tables["table"];
-                DV3.Sort = "ID desc";
-                DV3.RowFilter = "LBID=4";
-                this.GridView4.DataSource = DV3;
-                this.GridView4.DataBind();
-
-                DataView DV4 = new DataView();//班级活动回顾
-                DV4.Table = ds.Tables["table"];
-                DV4.Sort = "ID desc";
-                DV4.RowFilter = "LBID=5";
-                this.GridView5.DataSource = DV4;
-                this.GridView5.DataBind();
-                #endregion
-                #region
-                //DataTable table2 = new DataTable();
-                //SqlDataAdapter da2 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=2 order by ID desc", cn);//最新班级新闻
-                //da2.Fill(table2);
-                //this.GridView2.DataSource = table2;
+                //DataView DV1 = new DataView();//最新班级新闻
+                //DV1.Table = ds.Tables["table"];
+                //DV1.Sort = "ID desc";
+                //DV1.RowFilter = "LBID=2";
+                //this.GridView2.DataSource = DV1;
                 //this.GridView2.DataBind();
 
-                //DataTable table3 = new DataTable();
-                //SqlDataAdapter da3 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=3 order by ID desc", cn);//最新学习资料
-                //da3.Fill(table3);
-                //this.GridView3.DataSource = table3;
+                //DataView DV2 = new DataView();//最新学习资料
+                //DV2.Table = ds.Tables["table"];
+                //DV2.Sort = "ID desc";
+                //DV2.RowFilter = "LBID=3";
+                //this.GridView3.DataSource = DV2;
                 //this.GridView3.DataBind();
 
-                //DataTable table4 = new DataTable();
-                //SqlDataAdapter da4 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=4 order by ID desc", cn);//班级活动回顾
-                //da4.Fill(table4);
-                //this.GridView4.DataSource = table4;
+                //DataView DV3 = new DataView();//班级活动回顾
+                //DV3.Table = ds.Tables["table"];
+                //DV3.Sort = "ID desc";
+                //DV3.RowFilter = "LBID=4";
+                //this.GridView4.DataSource = DV3;
                 //this.GridView4.DataBind();
 
-                //DataTable table5 = new DataTable();
-                //SqlDataAdapter da5 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=5 order by ID desc", cn);//友情链接
-                //da5.Fill(table5);
-                //this.GridView5.DataSource = table5;
+                //DataView DV4 = new DataView();//班级活动回顾
+                //DV4.Table = ds.Tables["table"];
+                //DV4.Sort = "ID desc";
+                //DV4.RowFilter = "LBID=5";
+                //this.GridView5.DataSource = DV4;
                 //this.GridView5.DataBind();
+                #endregion
+                #region
+                DataTable table2 = new DataTable();
+                SqlDataAdapter da2 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=2 order by ID desc", cn);//最新班级新闻
+                da2.Fill(table2);
+                this.GridView2.DataSource = table2;
+                this.GridView2.DataBind();
+
+                DataTable table3 = new DataTable();
+                SqlDataAdapter da3 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=3 order by ID desc", cn);//最新学习资料
+                da3.Fill(table3);
+                this.GridView3.DataSource = table3;
+                this.GridView3.DataBind();
+
+                DataTable table4 = new DataTable();
+                SqlDataAdapter da4 = new SqlDataAdapter("select top 5 ID,LBID,Title from MainPage where LBID=4 order by ID desc", cn);//班级活动回顾
+                da4.Fill(table4);
+                this.GridView4.DataSource = table4;
+                this.GridView4.DataBind();
+
+                DataTable table5 = new DataTable();
+                SqlDataAdapter da5 = new SqlDataAdapter("select top 5 ID,LBID,Title,text from MainPage where LBID=5 order by ID desc", cn);//友情链接
+                da5.Fill(table5);
+                this.GridView5.DataSource = table5;
+                this.GridView5.DataBind();
                 #endregion
 
             }
-
-
-
-
 
             this.Label5.Text = "你是第" + Session["count"] + "位访问者";
 
@@ -113,8 +108,13 @@ namespace computer2011
 
         protected void LinkButton7_Click(object sender, EventArgs e)
         {
+
             string ID = ((Label)((LinkButton)sender).Parent.Parent.Controls[0].FindControl("Label16")).Text;
-            Response.Redirect("Main_Content.aspx?ID=" + ID + "");
+            SqlCommand cmd = new SqlCommand("select text  from MainPage where id="+ID+"",cn);
+            cn.Open();
+            
+            Response.Redirect(""+cmd.ExecuteScalar()+"");
+            cn.Close();
         }
 
         protected void Button1_Click(object sender, EventArgs e)//登录
