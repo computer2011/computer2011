0654a96ca69c3f211532b7cb03ca0f8f4f22a0f7
DongQiPeng <747859839@qq.com>
2014-05-13 10:26:56 +0800

    dqp10:26

--- LHPF.cs
+++ LHPF.cs
@@ -11,22 +11,12 @@ namespace computer2011
     public class LHPF
     {
         SqlConnection cn = new SqlConnection(new ConnectDatabase().conn);
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public static void  LH(string XN)
-        {
-=======
-        public static void LH(string XN)
-        {
-            SqlConnection cn = new SqlConnection(new ConnectDatabase().conn);
->>>>>>> b52341142da66b3827ce914777560d935d17e9b9
-=======
 
         public static void LH(string XN)
         {
             SqlConnection cn = new SqlConnection(new ConnectDatabase().conn);
 
->>>>>>> ab8dc0caf7d3831005237aea4f8cf3b4aa644342
+
             string LHPF = "insert into Student_LHPF (Sno, XN, XID, DF, DFYJ) select Sno,XN, 7 as xid, SUM(jd*xf)/sum(XF)*50/4 as df, SUM(jd*xf)/sum(XF) as DFYJ from Student_CJ group by Sno,XN where XN='" + XN + "'";
             SqlCommand CM = new SqlCommand();
             CM.CommandText = LHPF;
