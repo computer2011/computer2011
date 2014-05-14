27997b8ca5db6ce7a6eeb77d578a5c484430f6c5
DongQiPeng <747859839@qq.com>
2014-05-14 12:57:21 +0800

    dqp12

--- StuInfo.aspx.designer.cs
+++ StuInfo.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace computer2011 {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator10;
         
         /// <summary>
-        /// RegularExpressionValidator2 控件。
-        /// </summary>
-        /// <remarks>
-        /// 自动生成的字段。
-        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
-        
-        /// <summary>
         /// TextBox4 控件。
         /// </summary>
         /// <remarks>
